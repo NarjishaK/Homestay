6b8caa14b455569d90bb3ed28a1c442f654bb99d
unknown <navaskuniyil6@gmail.com>
2024-05-02 13:30:20 +0530

    server folder convert to another folder

--- server/modals/category-model.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const categorySchema = mongoose.Schema({
-  name: { type: String, required: true },
-  image: { type: String, required: true },
-});
-
-const categoryList = mongoose.model("categoryList", categorySchema);
-module.exports = categoryList;
